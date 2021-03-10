db3af44d044f266be2f6e5a2f843ac3f69915bb4
Devon Hanses <dhansen@lcs.on.ca>
2021-03-10 10:03:27 -0500

    Initial Commit

---
+++ Culminating Task/Culminating_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Culminating_TaskApp.swift
+//  Culminating Task
+//
+//  Created by Devon Hansen on 2021-03-10.
+//
+
+import SwiftUI
+
+@main
+struct Culminating_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
